6122127414247c1a0d9b56c558324120998286e1
byRafi <r.dawidczyk@outlook.de>
2025-09-14 19:54:54 +0200

    Add files via upload

---
+++ trainingsplan/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ExcelPlan from "./ExcelPlan";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: 'center' }}>⚽ Trainingsplan</h1>
+      <ExcelPlan />
+    </div>
+  );
+}
+
+export default App;
